97467790276697d4ff2e5a69c8e83d05ea87a0d8
josue22almo <josue.almo@goin.team>
2019-01-16 14:02:12 +0100

    adding ajv to validate object errors

--- src/cpm.ts
+++ src/cpm.ts
@@ -1,6 +1,7 @@
 
 import { MyActivity, MyActivityList, IActivityConfig } from "./classes";
 import fs = require("fs");
+import Ajv = require("ajv");
 
 if (!process.argv[2]) {
   console.log("Provide an input file to run this code");
@@ -15,8 +16,49 @@ if (!data.activities) {
 }
 
 const activityList = new MyActivityList();
+const schema = {
+  name: "activity-schema",
+  type: "object",
+  properties: {
+    id: {
+      type: "string",
+    },
+    duration: {
+      type: "number",
+    },
+    cost: {
+      type: "number",
+    },
+    maxWorkers: {
+      type: "number",
+    },
+    successors: {
+      type: "array",
+      items: {
+        type: "string",
+      },
+    },
+    predecessors: {
+      type: "array",
+      items: {
+        type: "string",
+      },
+    },
+  },
+  required: ["id", "duration", "cost", "maxWorkers", "successors", "predecessors"],
+};
+
+const myajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
+const validate = myajv.compile(schema);
 
 for (const activityConfig of data.activities) {
+  const valid = validate(activityConfig);
+  if (!valid) {
+    console.log("Schema errors with activity:", activityConfig, {
+      errors: validate.errors,
+    });
+    process.exit(0);
+  }
   const act = new MyActivity(activityConfig);
   activityList.addActivity(act);
 }
